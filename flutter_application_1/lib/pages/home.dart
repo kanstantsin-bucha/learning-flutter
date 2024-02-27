110595761d8b076de8130f17e8f338f4c97474c8
Kanstantsin Bucha <kanstantsin.bucha@withintent.com>
2024-02-27 16:38:48 +0100

    The app nav bar has tappable buttons.

--- flutter_application_1/lib/pages/home.dart
+++ flutter_application_1/lib/pages/home.dart
@@ -7,24 +7,58 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          'Breakfast',
-          style: TextStyle(
-            color: Colors.black,
-            fontSize: 18,
-            fontWeight: FontWeight.bold
-          ),
-        ),
-        leading: Container(
-          margin: const EdgeInsets.all(10),
-          decoration: BoxDecoration(
-            color: const Color(0xffF7F8F8),
-            borderRadius: BorderRadius.circular(10)
-          ),
-          child: SvgPicture.asset('assets/icons/Arrow - Left 2.svg'),
+      appBar: createAppBar(),
+    );
+  }
+
+  AppBar createAppBar() {
+    return AppBar(
+      title: const Text(
+        'Breakfast',
+        style: TextStyle(
+          color: Colors.black,
+          fontSize: 18,
+          fontWeight: FontWeight.bold
         ),
       ),
+      leading: createBackButton(),
+      actions: [ createMenuButton() ],
     );
+  }
+
+  GestureDetector createMenuButton() {
+    return GestureDetector(
+      onTap: () { print("Tap Menu"); },
+      child: Container(
+        margin: const EdgeInsets.all(10),
+        alignment: Alignment.center,
+        width: 37,
+        decoration: BoxDecoration(
+          color: const Color(0xFFE2E3E3),
+          borderRadius: BorderRadius.circular(10)
+        ),
+        child: SvgPicture.asset(
+          'assets/icons/dots.svg',
+          width: 5,
+          height: 5
+        ),
+      )
+    );
+    
+  }
+
+  GestureDetector createBackButton() {
+    return GestureDetector(
+        onTap: () { print("Tap Back"); },
+        child: Container(
+          margin: const EdgeInsets.all(10),
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+              color: const Color(0xFFE2E3E3),
+              borderRadius: BorderRadius.circular(10)),
+          child: SvgPicture.asset('assets/icons/Arrow - Left 2.svg',
+              width: 26, height: 26),
+        )
+      );
   }
 }
